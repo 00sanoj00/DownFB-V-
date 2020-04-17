3e49f444708cd17e65d665ed345441fbebe7c054
00sanoj00 <00sanoj00@gmail.com>
2020-04-18 03:21:14 +0530

    Mega Update
    add main disgne
    add fb yt and any online video download option
    add new themes
    fix size
    fizx text colour

--- DownFB-V-/DownFB-V-/Main.Designer.cs
+++ DownFB-V-/DownFB-V-/Main.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ghostTheme1 = new GhostTheme();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ghostButton4 = new GhostButton();
+            this.ghostTextBox3 = new GhostTextBox();
+            this.ghostTextBox2 = new GhostTextBox();
+            this.ghostButton3 = new GhostButton();
+            this.ghostButton2 = new GhostButton();
+            this.ghostButton1 = new GhostButton();
+            this.ghostTheme1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ghostTheme1
+            // 
+            this.ghostTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ghostTheme1.Colors = new Bloom[0];
+            this.ghostTheme1.Controls.Add(this.textBox1);
+            this.ghostTheme1.Controls.Add(this.ghostButton4);
+            this.ghostTheme1.Controls.Add(this.ghostTextBox3);
+            this.ghostTheme1.Controls.Add(this.ghostTextBox2);
+            this.ghostTheme1.Controls.Add(this.ghostButton3);
+            this.ghostTheme1.Controls.Add(this.ghostButton2);
+            this.ghostTheme1.Controls.Add(this.ghostButton1);
+            this.ghostTheme1.Customization = "";
+            this.ghostTheme1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ghostTheme1.Font = new System.Drawing.Font("Verdana", 8F);
+            this.ghostTheme1.Image = null;
+            this.ghostTheme1.Location = new System.Drawing.Point(0, 0);
+            this.ghostTheme1.Movable = true;
+            this.ghostTheme1.Name = "ghostTheme1";
+            this.ghostTheme1.NoRounding = false;
+            this.ghostTheme1.ShowIcon = false;
+            this.ghostTheme1.Sizable = true;
+            this.ghostTheme1.Size = new System.Drawing.Size(652, 261);
+            this.ghostTheme1.SmartBounds = true;
+            this.ghostTheme1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.ghostTheme1.TabIndex = 0;
+            this.ghostTheme1.Text = "DownFB-V-";
+            this.ghostTheme1.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.ghostTheme1.Transparent = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(12, 112);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(627, 137);
+            this.textBox1.TabIndex = 7;
+            // 
+            // ghostButton4
+            // 
+            this.ghostButton4.Color = System.Drawing.Color.Empty;
+            this.ghostButton4.Colors = new Bloom[0];
+            this.ghostButton4.Customization = "";
+            this.ghostButton4.EnableGlass = true;
+            this.ghostButton4.Font = new System.Drawing.Font("Verdana", 8F);
+            this.ghostButton4.Image = null;
+            this.ghostButton4.Location = new System.Drawing.Point(591, 37);
+            this.ghostButton4.Name = "ghostButton4";
+            this.ghostButton4.NoRounding = false;
+            this.ghostButton4.Size = new System.Drawing.Size(48, 24);
+            this.ghostButton4.TabIndex = 6;
+            this.ghostButton4.Text = ":::";
+            this.ghostButton4.Transparent = false;
+            // 
+            // ghostTextBox3
+            // 
+            this.ghostTextBox3.Customization = "/////wAAAP8AAAD/Wlpa/w==";
+            this.ghostTextBox3.Font = new System.Drawing.Font("Verdana", 8F);
+            this.ghostTextBox3.Image = null;
+            this.ghostTextBox3.Location = new System.Drawing.Point(472, 37);
+            this.ghostTextBox3.MaxLength = 32767;
+            this.ghostTextBox3.Multiline = false;
+            this.ghostTextBox3.Name = "ghostTextBox3";
+            this.ghostTextBox3.NoRounding = false;
+            this.ghostTextBox3.ReadOnly = false;
+            this.ghostTextBox3.Size = new System.Drawing.Size(113, 24);
+            this.ghostTextBox3.TabIndex = 5;
+            this.ghostTextBox3.Text = "ghostTextBox3";
+            this.ghostTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.ghostTextBox3.Transparent = false;
+            this.ghostTextBox3.UseSystemPasswordChar = false;
+            // 
+            // ghostTextBox2
+            // 
+            this.ghostTextBox2.Customization = "/////wAAAP8AAAD/Wlpa/w==";
+            this.ghostTextBox2.Font = new System.Drawing.Font("Verdana", 8F);
+            this.ghostTextBox2.Image = null;
+            this.ghostTextBox2.Location = new System.Drawing.Point(12, 37);
+            this.ghostTextBox2.MaxLength = 32767;
+            this.ghostTextBox2.Multiline = false;
+            this.ghostTextBox2.Name = "ghostTextBox2";
+            this.ghostTextBox2.NoRounding = false;
+            this.ghostTextBox2.ReadOnly = false;
+            this.ghostTextBox2.Size = new System.Drawing.Size(436, 24);
+            this.ghostTextBox2.TabIndex = 4;
+            this.ghostTextBox2.Text = "ghostTextBox2";
+            this.ghostTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.ghostTextBox2.Transparent = false;
+            this.ghostTextBox2.UseSystemPasswordChar = false;
+            // 
+            // ghostButton3
+            // 
+            this.ghostButton3.Color = System.Drawing.Color.Empty;
+            this.ghostButton3.Colors = new Bloom[0];
+            this.ghostButton3.Customization = "";
+            this.ghostButton3.EnableGlass = true;
+            this.ghostButton3.Font = new System.Drawing.Font("Verdana", 8F);
+            this.ghostButton3.Image = null;
+            this.ghostButton3.Location = new System.Drawing.Point(12, 67);
+            this.ghostButton3.Name = "ghostButton3";
+            this.ghostButton3.NoRounding = false;
+            this.ghostButton3.Size = new System.Drawing.Size(628, 39);
+            this.ghostButton3.TabIndex = 3;
+            this.ghostButton3.Text = "ghostButton3";
+            this.ghostButton3.Transparent = false;
+            this.ghostButton3.Click += new System.EventHandler(this.ghostButton3_Click);
+            // 
+            // ghostButton2
+            // 
+            this.ghostButton2.Color = System.Drawing.Color.Empty;
+            this.ghostButton2.Colors = new Bloom[0];
+            this.ghostButton2.Customization = "";
+            this.ghostButton2.EnableGlass = true;
+            this.ghostButton2.Font = new System.Drawing.Font("Verdana", 8F);
+            this.ghostButton2.Image = null;
+            this.ghostButton2.Location = new System.Drawing.Point(586, 3);
+            this.ghostButton2.Name = "ghostButton2";
+            this.ghostButton2.NoRounding = false;
+            this.ghostButton2.Size = new System.Drawing.Size(24, 17);
+            this.ghostButton2.TabIndex = 1;
+            this.ghostButton2.Text = "-";
+            this.ghostButton2.Transparent = false;
+            this.ghostButton2.Click += new System.EventHandler(this.ghostButton2_Click);
+            // 
+            // ghostButton1
+            // 
+            this.ghostButton1.Color = System.Drawing.Color.Empty;
+            this.ghostButton1.Colors = new Bloom[0];
+            this.ghostButton1.Customization = "";
+            this.ghostButton1.EnableGlass = true;
+            this.ghostButton1.Font = new System.Drawing.Font("Verdana", 8F);
+            this.ghostButton1.Image = null;
+            this.ghostButton1.Location = new System.Drawing.Point(616, 3);
+            this.ghostButton1.Name = "ghostButton1";
+            this.ghostButton1.NoRounding = false;
+            this.ghostButton1.Size = new System.Drawing.Size(24, 17);
+            this.ghostButton1.TabIndex = 0;
+            this.ghostButton1.Text = "X";
+            this.ghostButton1.Transparent = false;
+            this.ghostButton1.Click += new System.EventHandler(this.ghostButton1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 261);
+            this.Controls.Add(this.ghostTheme1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ghostTheme1.ResumeLayout(false);
+            this.ghostTheme1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GhostTheme ghostTheme1;
+        private GhostButton ghostButton2;
+        private GhostButton ghostButton1;
+        private GhostButton ghostButton4;
+        private GhostTextBox ghostTextBox3;
+        private GhostTextBox ghostTextBox2;
+        private GhostButton ghostButton3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- DownFB-V-/DownFB-V-/Main.cs
+++ DownFB-V-/DownFB-V-/Main.cs
@@ -2,19 +2,111 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IronPython.Hosting;
+using IronPython.Compiler;
 
 namespace DownFB_V_
 {
-    public partial class Main : Form
+    public partial class Main : Form  
     {
+        StreamWriter stdin = null;
+
         public Main()
         {
             InitializeComponent();
         }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ghostButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        private void ghostButton2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void StartCmdProcess()
+        {
+            ProcessStartInfo pStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "start /WAIT",
+                WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            Process cmdProcess = new Process
+            {
+                StartInfo = pStartInfo,
+                EnableRaisingEvents = true,
+            };
+
+            cmdProcess.Start();
+            cmdProcess.BeginErrorReadLine();
+            cmdProcess.BeginOutputReadLine();
+            stdin = cmdProcess.StandardInput;
+
+            cmdProcess.OutputDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new MethodInvoker(() =>
+                    {
+                        textBox1.AppendText(evt.Data + Environment.NewLine);
+                        textBox1.ScrollToCaret();
+                        ghostTextBox2.Text = "";
+                    }));
+                }
+            };
+
+            cmdProcess.ErrorDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new Action(() =>
+                    {
+                        //rtbStdErr.AppendText(evt.Data + Environment.NewLine);
+                        //rtbStdErr.ScrollToCaret();
+                    }));
+                }
+            };
+
+            cmdProcess.Exited += (s, evt) =>
+            {
+                // cmdProcess?.Dispose();
+            };
+        }
+
+        private void ghostButton3_Click(object sender, EventArgs e)
+        {
+          
+
+            textBox1.ForeColor = Color.LawnGreen;
+            textBox1.Text = "";
+            StartCmdProcess();
+            //stdin.WriteLine();
+           stdin.Write(ghostTextBox2.Text + Environment.NewLine);
+            stdin.WriteLine();
+
+
+           }
     }
 }
